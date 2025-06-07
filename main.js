3e0ed6a4cea805474e974c8f58df80ff51b8b616
Antlered-Snek <118801348+Antlered-Snek@users.noreply.github.com>
2025-06-07 20:01:18 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,210 @@
+
+
+
+
+// Lock Orientation
+screen.orientation.lock("landscape")
+
+// Elements
+let y_stick = document.getElementById('y_control').getElementsByClassName('handle')[0];
+let x_stick = document.getElementById('x_control').getElementsByClassName('handle')[0];
+let numpad = document.getElementById('numpad')
+let showcode_screen = document.getElementById('showcode')
+let missile_launch = document.getElementById('missile-launch')
+
+// Element Constants
+y_stick.style.top = "255px"
+x_stick.style.left = "255px"
+
+numpad.style.display = 'none'
+
+
+// Global Variables
+let joystick = {
+	x: 0,
+	y: 0
+}
+
+let touchObjects = {
+	x: null,
+	y: null
+}
+
+let isFollowingY = false
+let isFollowingX = false
+let missileIsOn = false
+
+let password = '6942'
+let currentCodeIndex = 0
+
+
+// Functions
+function updateY(deltaY, isTouch=false) {
+	// Localize Delta
+	if (isTouch) {
+		let box = y_stick.getBoundingClientRect()
+		deltaY -= Math.floor(box.y+box.height*0.5)
+	}
+
+	// Regular Method
+	if (Math.abs(joystick.y - deltaY) < 255) {
+		// Update Joystick Value
+		joystick.y -= deltaY
+
+		// Update HTML
+		let offset = parseInt(y_stick.style.top.substring(0, y_stick.style.top.length-2)) + deltaY
+		y_stick.style.top = `${offset}px`
+	}
+
+	//  Overflow
+	else {
+		if 		(deltaY<0) {
+			joystick.y = 255
+			y_stick.style.top = "0px"
+		}
+		else if (deltaY>0) {
+			joystick.y = -255
+			y_stick.style.top = "510px"
+		}
+	}
+}
+
+function updateX(deltaX, isTouch=false) {
+	// Localize Delta
+	if (isTouch) {
+		let box = x_stick.getBoundingClientRect()
+		deltaX -= Math.floor(box.x+box.width*0.5)
+	}
+	
+	// Regular Method
+	if (Math.abs(joystick.x + deltaX) < 255) {
+		// Update Joystick Value
+		joystick.x += deltaX
+
+		// Update HTML
+		let offset = parseInt(x_stick.style.left.substring(0, x_stick.style.left.length-2)) + deltaX
+		x_stick.style.left = `${offset}px`
+	}
+
+	//  Overflow
+	else {
+		if 		(deltaX>0) {
+			joystick.x = 255
+			x_stick.style.left = "510px"
+		}
+		else if (deltaX<0) {
+			joystick.x = -255
+			x_stick.style.left = "0px"
+		}
+	}
+}
+
+
+// Event Listeners
+	// Mouse Events
+document.getElementById('y_control').addEventListener('mousedown', e => {
+	isFollowingY = true
+})
+document.getElementById('x_control').addEventListener('mousedown', e => {
+	isFollowingX = true
+})
+
+document.addEventListener('mouseup', e => {
+	isFollowingY = false
+	isFollowingX = false
+})
+
+document.addEventListener('mousemove', e => {
+	if (isFollowingY) updateY(e.movementY)
+	if (isFollowingX) updateX(e.movementX)
+})
+
+	// Touch Events
+document.getElementById('y_control').addEventListener('touchstart', e => {
+	isFollowingY = true
+	touchObjects.y = e.changedTouches[0]
+	updateY(Math.floor(touchObjects.y.clientY), true)
+})
+document.getElementById('x_control').addEventListener('touchstart', e => {
+	isFollowingX = true
+	touchObjects.x = e.changedTouches[0]
+	updateX(Math.floor(touchObjects.x.clientX), true)
+})
+
+document.addEventListener('touchend', e => {
+	for (let i=0; i<e.changedTouches.length; i++) {
+		if (isFollowingY) {
+			if (e.changedTouches[i].identifier == touchObjects.y.identifier) {
+				isFollowingY = false
+				touchObjects.y = null
+				updateY(joystick.y)
+			}	
+		}
+		
+		if (isFollowingX) {
+			if (e.changedTouches[i].identifier == touchObjects.x.identifier) {
+				isFollowingX = false
+				touchObjects.x = null
+				updateX(-joystick.x)
+			}	
+		}
+	}
+})
+
+document.addEventListener('touchmove', e => {
+	for (let i=0; i<e.changedTouches.length; i++) {
+		if (isFollowingY && e.changedTouches[i].identifier == touchObjects.y.identifier) updateY(Math.floor(e.changedTouches[i].clientY), true)
+		if (isFollowingX && e.changedTouches[i].identifier == touchObjects.x.identifier) updateX(Math.floor(e.changedTouches[i].clientX), true)
+	}
+})
+
+	// Buttons
+document.getElementById('missile-prep').addEventListener('click', e => {
+	if (missileIsOn) {
+		missile_launch.style.display = "none"
+		missileIsOn = false
+	}
+	else {
+		numpad.style.display = 'grid'
+	}
+})
+
+missile_launch.addEventListener('click', e => {
+	alert("PHEWWWWWWWWWWWW")
+	missile_launch.style.display = "none"
+	missileIsOn = false
+})
+
+document.getElementById('accept-numpad').addEventListener('click', e => {
+	numpad.style.display = 'none'
+
+	// Check Password
+	if (password == showcode_screen.getElementsByTagName('div')[0].innerHTML + showcode_screen.getElementsByTagName('div')[1].innerHTML + showcode_screen.getElementsByTagName('div')[2].innerHTML + showcode_screen.getElementsByTagName('div')[3].innerHTML) {
+		missileIsOn = true
+		missile_launch.style.display = "block"
+	}
+	else {
+		alert('Incorrect Password')
+		missileIsOn = false
+	}
+
+	// Reset
+	for (let i=0; i<4; i++) {
+		showcode_screen.getElementsByTagName('div')[i].innerHTML = '_'
+	}
+	currentCodeIndex = 0;
+})
+
+	// Numpad
+for (let i=1; i<10; i++) {
+	numpad.getElementsByTagName('div')[i+5].addEventListener('click', e => {
+		
+		if (currentCodeIndex < 4) {
+			showcode_screen.getElementsByTagName('div')[currentCodeIndex].innerHTML = i
+			
+			// Increment
+			currentCodeIndex++
+		}
+
+	})
+}
